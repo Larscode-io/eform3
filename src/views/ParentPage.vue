713e2078b160ef53ba62eef2e7a0d95bb795b1df
Lars Devocht <lars.devocht@gmail.com>
2024-04-13 13:56:32 +0200

    testing flex grow and shrink

--- src/views/ParentPage.vue
+++ src/views/ParentPage.vue
@@ -2,6 +2,16 @@
     <div>
         <MyButton variant="primary" @click="handleButtonClick">Click Me</MyButton>
     </div>
+    <div class="flex w-364"> <!-- Container has limited width -->
+        <div class="p-4 text-white bg-red-500 shrink-0">Item 1 (does not shrink)</div>
+        <div class="p-4 text-white bg-green-500">Item 2 (may shrink if needed)</div>
+        <div class="p-4 text-white bg-blue-500">Item 3 (may shrink if needed)</div>
+    </div>
+    <div class="flex w-full">
+        <div class="p-4 text-white bg-red-500">Item 1</div>
+        <div class="flex-grow p-4 text-white bg-green-500">Item 2 (grows to use available space)</div>
+        <div class="p-4 text-white bg-blue-500">Item 3</div>
+    </div>
 </template>
 
 <script setup>
