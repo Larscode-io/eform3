039908c63697e515fab81ac9d24d591d53fa996a
Lars Devocht <lars.devocht@gmail.com>
2024-04-13 13:54:36 +0200

    added view for user details

---
+++ src/components/FormUserDetails.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+const firstName = ref('');
+const lastName = ref('');
+const email = ref('');
+const continueForm = () => {
+    console.log('Continue');
+};
+</script>
+
+<template>
+    <div>
+        <h1>User Details</h1>
+        <form>
+            <div class="form-group">
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" v-model="firstName" />
+            </div>
+            <div class="form-group">
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" v-model="lastName" />
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" v-model="email" />
+            </div>
+            <button @click="continueForm">Continue</button>
+        </form>
+    </div>
+</template>
