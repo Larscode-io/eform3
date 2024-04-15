11fdf607e4bd39b56fe4ea0b7272fb966199c127
Lars Devocht <lars.devocht@gmail.com>
2024-04-15 21:07:24 +0200

    added page to show and confirm all entered data

---
+++ src/components/FormConfirmation.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+    form: Object,
+});
+const componentName = 'FormConfirmation';
+
+console.log('FormConfirmation', props.form);
+</script>
+
+<!-- // hier parent data tonen en laten bevestigen -->
+<template>
+    <div>
+        <p>{{ componentName }}</p>
+        <div class="p-4 border border-gray-300 rounded">
+            <h2 class="text-xl font-bold">Bevestiging</h2>
+            <pre>{{ JSON.stringify(form, null, 2) }}</pre>
+            <p>Uw gegevens zijn correct ingevuld. Klik op 'Verzenden' om uw documenten in te dienen.</p>
+        </div>
+    </div>
+</template>
