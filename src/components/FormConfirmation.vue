64868888cf12be9c1e321606e2a2872a76318a95
Lars Devocht <lars.devocht@gmail.com>
2024-04-16 22:35:56 +0200

    change confirmation email address if needed

--- src/components/FormConfirmation.vue
+++ src/components/FormConfirmation.vue
@@ -1,10 +1,15 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+const receiveConfirmation = ref(false);
 const props = defineProps({
     form: Object,
 });
+const confirmationEmail = ref('');
 const componentName = 'FormConfirmation';
+onMounted(() => {
+    confirmationEmail.value = props.form.email;
 
-console.log('FormConfirmation', props.form);
+});
 </script>
 
 <!-- // hier parent data tonen en laten bevestigen -->
@@ -14,7 +19,16 @@ console.log('FormConfirmation', props.form);
         <div class="p-4 border border-gray-300 rounded">
             <h2 class="text-xl font-bold">Bevestiging</h2>
             <pre>{{ JSON.stringify(form, null, 2) }}</pre>
-            <p>Uw gegevens zijn correct ingevuld. Klik op 'Verzenden' om uw documenten in te dienen.</p>
+            <p>Zijn uw gegevens correct ingevuld? Klik op 'Verzenden' om uw documenten in te dienen.</p>
+            <label>Klik hier als u een bevestiging van de indiening wilt ontvangen:
+                <input type="checkbox" v-model="receiveConfirmation"
+                    class="w-4 h-4 text-blue-600 bg-transparent border-gray-300" />
+                <div v-if="receiveConfirmation">
+                    <input type="email" placeholder="Vul hier uw e-mailadres in" :value="props.form.email"
+                        @input="$emit('update', { email: $event.target.value })"
+                        class="w-full px-2 py-1 mt-2 border border-gray-300 rounded" />
+                </div>
+            </label>
         </div>
     </div>
 </template>
