1f87ffc1162fdfa33ca48759b68d378aed2bb93e
Lars Devocht <lars.devocht@gmail.com>
2024-04-16 22:54:26 +0200

    v-model instead separate :value and @input

--- src/components/FormConfirmation.vue
+++ src/components/FormConfirmation.vue
@@ -1,14 +1,15 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-const receiveConfirmation = ref(false);
+import { ref, watch } from 'vue';
 const props = defineProps({
     form: Object,
 });
+const emit = defineEmits(['update']);
 const confirmationEmail = ref('');
+const receiveConfirmation = ref(false);
 const componentName = 'FormConfirmation';
-onMounted(() => {
-    confirmationEmail.value = props.form.email;
 
+watch(confirmationEmail, (newVal) => {
+    emit('update', { confirmationEmail: newVal });
 });
 </script>
 
@@ -18,14 +19,13 @@ onMounted(() => {
         <p>{{ componentName }}</p>
         <div class="p-4 border border-gray-300 rounded">
             <h2 class="text-xl font-bold">Bevestiging</h2>
-            <pre>{{ JSON.stringify(form, null, 2) }}</pre>
+            <pre>{{ JSON.stringify(props.form, null, 2) }}</pre>
             <p>Zijn uw gegevens correct ingevuld? Klik op 'Verzenden' om uw documenten in te dienen.</p>
             <label>Klik hier als u een bevestiging van de indiening wilt ontvangen:
                 <input type="checkbox" v-model="receiveConfirmation"
                     class="w-4 h-4 text-blue-600 bg-transparent border-gray-300" />
                 <div v-if="receiveConfirmation">
-                    <input type="email" placeholder="Vul hier uw e-mailadres in" :value="props.form.email"
-                        @input="$emit('update', { confirmationEmail: $event.target.value })"
+                    <input type="text" placeholder="Vul hier uw e-mailadres in" v-model="confirmationEmail"
                         class="w-full px-2 py-1 mt-2 border border-gray-300 rounded" />
                 </div>
             </label>
